e06191fe17cdeced8c862a1386ac34b12506b6e7
Presson-coder <pressonzhou04@gmail.com>
2026-01-20 12:42:22 +0200

    neon db

--- config/database.js
+++ config/database.js
@@ -1,22 +1,19 @@
 import { Pool } from "pg";
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=require`;
-
 const pool = new Pool({
-  connectionString,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
 
-console.log('connectionString:', connectionString);
-
 export const query = (text, params) => pool.query(text, params);
 
 export const connectToDatabase = async () => {
   try {
-    await pool.connect();
+    const client = await pool.connect();
     console.log("Connected to the PostgreSQL database");
+    client.release();
   } catch (error) {
     console.error("Database connection error:", error);
     throw error;
